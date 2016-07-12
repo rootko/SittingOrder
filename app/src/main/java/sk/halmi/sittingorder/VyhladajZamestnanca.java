f287fce8e2be05fbdef193174fe2ce7ae3f2b9ae
andreahh <huzvarova.andrea@gmail.com>
2016-07-12 14:28:23 +0200

    Prve zlucenie- aktivita Vyhladaj zamestnanca

---
+++ app/src/main/java/sk/halmi/sittingorder/VyhladajZamestnanca.java
@@ -0,0 +1,99 @@
+package sk.halmi.sittingorder;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class VyhladajZamestnanca extends AppCompatActivity {
+
+    EditText meno;
+    EditText priezvisko;
+    TextView vysledok;
+
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editujmiestnost);
+        meno = (EditText)findViewById(R.id.ETmeno);
+        priezvisko = (EditText)findViewById(R.id.ETpriezvisko);
+        vysledok = (TextView)findViewById(R.id.TVvysledok);
+
+        Button potvrd = (Button)findViewById(R.id.enter);
+        potvrd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name= meno.getText().toString();
+                String surname = priezvisko.getText().toString();
+                vysledok.setText(name + " " + surname);
+            }
+        });
+
+        Button zrus = (Button)findViewById(R.id.zrus);
+        zrus.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view){
+                vysledok.setText("nic ste nezadali");
+            }
+        }
+        );
+    }
+
+
+
+//    public void buttonClicked(View button){
+//        String MENO = dajMeno(R.id.ETmeno);
+//        String PRIEZVISKO = dajPriezvisko(R.id.ETpriezvisko);
+//    }
+
+//    //funkcia aby som dostala meno
+//    protected String dajMeno(ETmeno)
+//    {
+//        EditText etVstup = ((EditText)findViewById(ETmeno));
+//        String strVstup = etVstup.getText().toString();
+//        String nMeno;
+//        try
+//        {
+//            nMeno = strVstup;
+//        }
+//   catch();
+//        return nMeno;
+//    }
+//
+//   //funkcia aby som dostala priezvisko
+//    protected String dajPriezvisko(ETpriezvisko)
+//    {
+//        EditText etVstup2 = ((EditText)findViewById(ETpriezvisko));
+//        String strVstup2 = etVstup2.getText().toString();
+//        String nPriezvisko;
+//        try
+//        {
+//            nPriezvisko = strVstup2;
+//        }
+//        return nPriezvisko;
+//    }
+//
+//    // TO DO- funkcia na spojenie meno a priezvisko a vypisalo ho
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+}
