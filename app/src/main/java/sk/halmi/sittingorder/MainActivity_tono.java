8328b0c85ad6fdc41a1fc1bf3b375eb0df11cead
Antonio Raffaele Iannaccone <antonio.iannaccone7@gmail.com>
2016-07-13 14:22:36 +0200

    MainActivity is ready to be sold!

--- app/src/main/java/sk/halmi/sittingorder/MainActivity_tono.java
+++ app/src/main/java/sk/halmi/sittingorder/MainActivity_tono.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -17,8 +18,15 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import sk.halmi.sittingorder.adapter.MiestnostAdapter_tono;
+import sk.halmi.sittingorder.api.BackendAPI;
+import sk.halmi.sittingorder.api.SittingOrder;
 import sk.halmi.sittingorder.api.model.Miestnost_tono;
+import sk.halmi.sittingorder.api.model.room.Result;
+import sk.halmi.sittingorder.api.model.room.RoomSet;
 
 public class MainActivity_tono extends AppCompatActivity {
 
@@ -34,10 +42,7 @@ public class MainActivity_tono extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(R.string.titlesearchroom);
 
-        Spinner budovaSpinner = (Spinner) findViewById(R.id.spinnerBudova);
-        ArrayAdapter<CharSequence> spinnerArrayAdapter = ArrayAdapter.createFromResource(getBaseContext(), R.array.budovaArray, android.R.layout.simple_spinner_item); //selected item will look like a spinner set from XML
-        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        budovaSpinner.setAdapter(spinnerArrayAdapter);
+        final Spinner budovaSpinner = (Spinner) findViewById(R.id.spinnerBudova);
 
         //Spinner selection
         budovaSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -62,6 +67,57 @@ public class MainActivity_tono extends AppCompatActivity {
 
         this.poschodieSpinnner = (Spinner) findViewById(R.id.spinnerPoschodie);
 
+        final String[] buildingIds = new String[] {"JT6", "JT8"};
+        final String[] poschodieIds = new String[] {"1", "2", "3", "4", "5", "6"};
+
+        poschodieSpinnner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                SittingOrder client = BackendAPI.createService(SittingOrder.class);
+
+                int index = budovaSpinner.getSelectedItemPosition();
+                int indexPoschodie = poschodieSpinnner.getSelectedItemPosition();
+
+                Toast.makeText(MainActivity_tono.this, buildingIds[index], Toast.LENGTH_SHORT).show();
+
+                String buildingFilter = "IdBuilding eq '" + buildingIds[index] + "'";
+                String floorFilter = "IdFloor eq '" + poschodieIds [indexPoschodie] + "'";
+                Call<RoomSet> call = client.getRooms(buildingFilter + " and " + floorFilter, "json");
+                call.enqueue(new Callback<RoomSet>() {
+                    @Override
+                    public void onResponse(Call<RoomSet> call, Response<RoomSet> response) {
+                        Log.d("OkHttp", "onResponse");
+                        if (response.isSuccessful()) {
+                            RoomSet roomSet = response.body();
+                            ArrayList<Miestnost_tono> data = new ArrayList<Miestnost_tono>();
+                            for (Result result : roomSet.getD().getResults()) {
+                                Log.d("OkHttp", result.getIdBuilding() + " " + result.getIdFloor() + " " + result.getIdRoom());
+                                data.add(new Miestnost_tono(result.getIdRoom(),result.getIdFloor()));
+                            }
+
+                            vytvorMiestnosti(data);
+
+                        } else {
+                            // error response, no access to resource?
+                            Log.d("OkHttp", "--------- didn't work ---------" + response.message());
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<RoomSet> call, Throwable t) {
+                        // something went completely south (like no internet connection)
+                        Log.d("OkHttp", "onFailure");
+                    }
+                });
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -72,18 +128,18 @@ public class MainActivity_tono extends AppCompatActivity {
             }
         });
         zoznamMiestnosti = (ListView) findViewById(R.id.zoznamMiestnosti);
-        vytvorMiestnosti();
+//        vytvorMiestnosti();
     }
 
 
-    private void vytvorMiestnosti() {
+    private void vytvorMiestnosti(ArrayList<Miestnost_tono> miestnosti) {
         // Construct the data source
-        final ArrayList<Miestnost_tono> arrayOfUsers = new ArrayList<Miestnost_tono>();
-        for (int i = 0; i < 10; i++) {
-            arrayOfUsers.add(new Miestnost_tono("Miestnost:" + i, "Poschodie:" + i));
-        }
-// Create the adapter to convert the array to views
-        MiestnostAdapter_tono adapter = new MiestnostAdapter_tono(this, arrayOfUsers);
+//        final ArrayList<Miestnost_tono> arrayOfUsers = new ArrayList<Miestnost_tono>();
+//        for (int i = 0; i < 10; i++) {
+//            arrayOfUsers.add(new Miestnost_tono("Miestnost:" + i, "Poschodie:" + i));
+//        }
+//// Create the adapter to convert the array to views
+        MiestnostAdapter_tono adapter = new MiestnostAdapter_tono(this, miestnosti);
 
         zoznamMiestnosti.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
