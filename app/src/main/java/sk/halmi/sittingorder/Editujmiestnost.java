c42af48449c4d45f8282d5661a88c555df5a6cc4
Lea user <Lea user>
2016-07-13 14:17:08 +0200

    dynamicke layouty, pajcurikovia, ...

--- app/src/main/java/sk/halmi/sittingorder/Editujmiestnost.java
+++ app/src/main/java/sk/halmi/sittingorder/Editujmiestnost.java
@@ -4,44 +4,57 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import sk.halmi.sittingorder.api.model.RowItemEdituj;
 
 public class Editujmiestnost extends AppCompatActivity {
 
     private List<RowItemEdituj> zoznammien = new ArrayList<RowItemEdituj>();
     List<RowItemEdituj> zmenamien = new ArrayList<RowItemEdituj>();
-    ListView listView;
-    FloatingActionButton fab;
-
+//    ListView listView;
+//    FloatingActionButton fab;
+    @Bind(R.id.list_wrapper)
+    LinearLayout wrapper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editujmiestnost1);
+        ButterKnife.bind(this);
 
-        listView=(ListView)findViewById(R.id.listView);
-
-        //FLOATING BUTTON
-        fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+//        listView=(ListView)findViewById(R.id.listView);
+        findViewById(R.id.b_test).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Toast.makeText(Editujmiestnost.this, "clicked", Toast.LENGTH_SHORT).show();
             }
         });
+
+//        //FLOATING BUTTON
+//        fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         // -------------------------- NAPLNANIE LISTVIEWU -----------------------------------------------
         zoznammien.add(new RowItemEdituj("1","Karol", "Kamo"));
@@ -50,65 +63,92 @@ public class Editujmiestnost extends AppCompatActivity {
         // ---------------------------- KAPACITA -------------------------------------------------------
 
         int velikost=zoznammien.size();
-        for (int j=0;j<10-velikost;j++)
+        for (int j=0;j<20-velikost;j++)
         {
             zoznammien.add(new RowItemEdituj("","", ""));
         }
 
         populateList();
         populateOnClickList();
-        saveList();
+        //saveList();
 
 
     }
 
-    //Kliknutie na floating button ULOZIT
-    private void saveList()
-    {
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int velkost=zoznammien.size();
-                if (velkost!=0)
-                {
-                    for (int i=0;i<velkost;i++)
-                    {
-                        RowItemEdituj rowItemEdituj = zoznammien.get(i);
-                        if(rowItemEdituj.getMeno().equals(""))
-                        {
-
-                        }
-                        else
-                        {
-                            RowItemEdituj rowItemEdituj_save = zoznammien.get(i);
-                            // posielanie na ulozenie potom
-                        }
-                    }
-                }}
-        });
-    }
+//    //Kliknutie na floating button ULOZIT
+//    private void saveList()
+//    {
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                int velkost=zoznammien.size();
+//                if (velkost!=0)
+//                {
+//                    for (int i=0;i<velkost;i++)
+//                    {
+//                        RowItemEdituj rowItemEdituj = zoznammien.get(i);
+//                        if(rowItemEdituj.getMeno().equals(""))
+//                        {
+//
+//                        }
+//                        else
+//                        {
+//                            RowItemEdituj rowItemEdituj_save = zoznammien.get(i);
+//                            // posielanie na ulozenie potom
+//                        }
+//                    }
+//                }}
+//        });
+//    }
 
     private void populateList() {
-        ArrayAdapter<RowItemEdituj> adapter = new ListViewAdapterRowItemEdituj();
-        listView.setAdapter(adapter);
+        LayoutInflater layoutInflater = LayoutInflater.from(Editujmiestnost.this);
+        Integer idenfier = 0;
+        for (RowItemEdituj item : zoznammien) {
+            //natiahni xml layout a vytvor objekt
+            View itemView = layoutInflater.inflate(R.layout.rowitem_edituj, null);
+            //priradime tag, aby sme ho vedeli volat neskor
+            itemView.setTag(idenfier);
+
+            //najdeme v R.layout.rowitem_edituj vsetky polia, ktore plnime
+            final EditText eID = ((EditText)itemView.findViewById(R.id.idecko));
+            final EditText eName = ((EditText)itemView.findViewById(R.id.name));
+            final EditText eSurname = ((EditText)itemView.findViewById(R.id.priezvisko));
+            eID.setText(item.getIdecko());
+            eName.setText(item.getMeno());
+            eSurname.setText(item.getPriezvisko());
+            itemView.findViewById(R.id.btndelete).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    eID.setText("");
+                    eName.setText("");
+                    eSurname.setText("");
+                }
+            });
+
+            wrapper.addView(itemView);
+        }
+//        ArrayAdapter<RowItemEdituj> adapter = new ListViewAdapterRowItemEdituj();
+//        listView.setAdapter(adapter);
     }
 
     private void populateOnClickList()
     {
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
-                                        {
-                                            @Override
-                                            public void onItemClick(AdapterView<?> parent, final View view, int position, long id)
-                                            {
-                                                int pos=position+1;
-                                                Toast.makeText(Editujmiestnost.this, Integer.toString(pos)+" Clicked", Toast.LENGTH_SHORT).show();}
-                                        }
-        );
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+//                                        {
+//                                            @Override
+//                                            public void onItemClick(AdapterView<?> parent, final View view, int position, long id)
+//                                            {
+//                                                int pos=position+1;
+//                                                Toast.makeText(Editujmiestnost.this, Integer.toString(pos)+" Clicked", Toast.LENGTH_SHORT).show();}
+//                                        }
+//        );
     }
 
     public class ListViewAdapterRowItemEdituj extends ArrayAdapter<RowItemEdituj> {
 
         public ListViewAdapterRowItemEdituj() {
+
             super(Editujmiestnost.this, R.layout.rowitem_edituj, zoznammien);
         }
 
--- app/src/main/java/sk/halmi/sittingorder/Vysledkyvyhladavania.java
+++ app/src/main/java/sk/halmi/sittingorder/Vysledkyvyhladavania.java
@@ -2,92 +2,145 @@ package sk.halmi.sittingorder;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
-;import sk.halmi.sittingorder.api.model.RowItem;
+;import butterknife.Bind;
+import butterknife.ButterKnife;
+import sk.halmi.sittingorder.api.model.RowItem;
+import sk.halmi.sittingorder.api.model.RowItemEdituj;
 
 public class Vysledkyvyhladavania extends Activity {
 
     private List<RowItem> vyhladavanie = new ArrayList<RowItem>();
-    ListView listView;
+    //    ListView listView;
+    @Bind(R.id.list_wrapper)
+    LinearLayout wrapper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vysledkyvyhladavania);
+        ButterKnife.bind(this);
 
-        listView=(ListView)findViewById(R.id.listView);
 
-        //------------------------------ NAPLNANIE LISTVIEWU ----------------------------------------------------
-        for (int i=0;i<6;i++) {
-            vyhladavanie.add(new RowItem("i","Karol", "Kamo", "8", "4", "409"));
+//        listView=(ListView)findViewById(R.id.listView);
+//        //------------------------------ NAPLNANIE LISTVIEWU ----------------------------------------------------
+        for (int i = 0; i < 26; i++) {
+            vyhladavanie.add(new RowItem("i", "Karol", "Kamo", "8", "4", "409"));
         }
         populateList();
-        populateOnClickList();
+//        populateOnClickList();
+
+
     }
 
     private void populateList() {
-        ArrayAdapter<RowItem> adapter = new ListViewAdapterRowItem();
-        listView.setAdapter(adapter);
-    }
 
-    private void populateOnClickList()
-    {
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
-                                        {
-                                            @Override
-                                            public void onItemClick(AdapterView<?> parent, final View view, int position, long id)
-                                            {
-                                                int pos=position+1;
-                                                Toast.makeText(Vysledkyvyhladavania.this, Integer.toString(pos)+" Clicked", Toast.LENGTH_SHORT).show();
+        LayoutInflater layoutInflater = LayoutInflater.from(Vysledkyvyhladavania.this);
+        Integer idenfier = 0;
+        for (RowItem item : vyhladavanie) {
+            //natiahni xml layout a vytvor objekt
+            View itemView = layoutInflater.inflate(R.layout.rowitem, null);
+            //priradime tag, aby sme ho vedeli volat neskor
+            itemView.setTag(idenfier);
 
-                                                startActivity(new Intent(Vysledkyvyhladavania.this,Editujmiestnost.class));
-                                            }
-                                        }
-        );
-    }
+            //najdeme v R.layout.rowitem_edituj vsetky polia, ktore plnime
+            TextView eID = ((TextView) itemView.findViewById(R.id.idecko));
+            TextView eName = ((TextView) itemView.findViewById(R.id.name));
+            TextView eSurname = ((TextView) itemView.findViewById(R.id.priezvisko));
+            TextView eBudova = ((TextView) itemView.findViewById(R.id.budova));
+            TextView ePoschodie = ((TextView) itemView.findViewById(R.id.poschodie));
+            TextView eMiestnost = ((TextView) itemView.findViewById(R.id.miestnost));
 
-    public class ListViewAdapterRowItem extends ArrayAdapter<RowItem> {
 
-        public ListViewAdapterRowItem() {
-            super(Vysledkyvyhladavania.this, R.layout.rowitem, vyhladavanie);
+            eID.setText(item.getIdecko());
+            eName.setText(item.getMeno());
+            eSurname.setText(item.getPriezvisko());
+            eBudova.setText(item.getBudova());
+            ePoschodie.setText(item.getPoschodie());
+            eMiestnost.setText(item.getMiestnost());
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(Vysledkyvyhladavania.this, " Clicked", Toast.LENGTH_SHORT).show();
+
+                    Intent intent=new Intent(Vysledkyvyhladavania.this,Editujmiestnost.class);
+                    startActivity(intent);
+
+                }
+            });
+
+            wrapper.addView(itemView);
+
+            // STARY KOD
+//        ArrayAdapter<RowItem> adapter = new ListViewAdapterRowItem();
+//        listView.setAdapter(adapter);
+//    }
+//
+//    private void populateOnClickList()
+//    {
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+//                                        {
+//                                            @Override
+//                                            public void onItemClick(AdapterView<?> parent, final View view, int position, long id)
+//                                            {
+//                                                int pos=position+1;
+//                                                Toast.makeText(Vysledkyvyhladavania.this, Integer.toString(pos)+" Clicked", Toast.LENGTH_SHORT).show();
+//
+//                                                startActivity(new Intent(Vysledkyvyhladavania.this,Editujmiestnost.class));
+//                                            }
+//                                        }
+//        );
         }
-
-        public View getView(int position, View covertView, ViewGroup parent) {
-            View itemView = covertView;
-            if (itemView == null) {
-                itemView = getLayoutInflater().inflate(R.layout.rowitem, parent, false);
-            }
-
-            RowItem rowItem = vyhladavanie.get(position);
-
-            TextView txt_idecko = (TextView) itemView.findViewById(R.id.idecko);
-            txt_idecko.setText(rowItem.getIdecko());
-
-            TextView txt_name = (TextView) itemView.findViewById(R.id.name);
-            txt_name.setText(rowItem.getMeno());
-
-            TextView txt_priezvisko = (TextView) itemView.findViewById(R.id.priezvisko);
-            txt_priezvisko.setText(rowItem.getPriezvisko());
-
-            TextView txt_budova = (TextView) itemView.findViewById(R.id.budova);
-            txt_budova.setText(rowItem.getBudova());
-
-            TextView txt_poschodie = (TextView) itemView.findViewById(R.id.poschodie);
-            txt_poschodie.setText(rowItem.getPoschodie());
-
-            TextView txt_miestnost = (TextView) itemView.findViewById(R.id.miestnost);
-            txt_miestnost.setText(rowItem.getMiestnost());
-
-            return itemView;
-        }
+//
+//    public class ListViewAdapterRowItem extends ArrayAdapter<RowItem> {
+//
+//        public ListViewAdapterRowItem() {
+//            super(Vysledkyvyhladavania.this, R.layout.rowitem, vyhladavanie);
+//        }
+//
+//        public View getView(int position, View covertView, ViewGroup parent) {
+//            View itemView = covertView;
+//            if (itemView == null) {
+//                itemView = getLayoutInflater().inflate(R.layout.rowitem, parent, false);
+//            }
+//
+//            RowItem rowItem = vyhladavanie.get(position);
+//
+//            TextView txt_idecko = (TextView) itemView.findViewById(R.id.idecko);
+//            txt_idecko.setText(rowItem.getIdecko());
+//
+//            TextView txt_name = (TextView) itemView.findViewById(R.id.name);
+//            txt_name.setText(rowItem.getMeno());
+//
+//            TextView txt_priezvisko = (TextView) itemView.findViewById(R.id.priezvisko);
+//            txt_priezvisko.setText(rowItem.getPriezvisko());
+//
+//            TextView txt_budova = (TextView) itemView.findViewById(R.id.budova);
+//            txt_budova.setText(rowItem.getBudova());
+//
+//            TextView txt_poschodie = (TextView) itemView.findViewById(R.id.poschodie);
+//            txt_poschodie.setText(rowItem.getPoschodie());
+//
+//            TextView txt_miestnost = (TextView) itemView.findViewById(R.id.miestnost);
+//            txt_miestnost.setText(rowItem.getMiestnost());
+//
+//            return itemView;
+//        }
+//    }
     }
 }
