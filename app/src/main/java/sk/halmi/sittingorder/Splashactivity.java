768ebf2f21bcab6c56423b3d882c92cf1a28a1d6
Lea user <Lea user>
2016-07-14 13:52:33 +0200

    Splash activity :))

---
+++ app/src/main/java/sk/halmi/sittingorder/Splashactivity.java
@@ -0,0 +1,34 @@
+package sk.halmi.sittingorder;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splashactivity extends AppCompatActivity {
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 1500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashactivity);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(Splashactivity.this, MainActivity_tono.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
